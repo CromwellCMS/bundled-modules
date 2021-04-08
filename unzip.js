a50bdef65b42bc0ad59bd57f2f7439fbe7ef81fc
elrix <xcom541@gmail.com>
2021-04-08 14:32:00 +0300

    update core

--- unzip.js
+++ unzip.js
@@ -1,4 +1,4 @@
-const decompress = require('decompress');
+const extractZip = require('extract-zip');
 const fs = require('fs-extra');
 const { dirname } = require('path');
 const glob = require('glob');
@@ -6,14 +6,13 @@ const normalizePath = require('normalize-path');
 
 ; (async () => {
     const files = glob.sync(`${normalizePath(__dirname)}/**/*.zip`);
-
     for (const zipName of files) {
         if (fs.pathExistsSync(zipName)) {
             try {
-                await decompress(zipName, dirname(zipName));
+                await extractZip(zipName, { dir: dirname(zipName) });
             } catch (err) {
                 try {
-                    await decompress(zipName, dirname(zipName));
+                    await extractZip(zipName, { dir: dirname(zipName) });
                 } catch (err) {
                     console.log(`Failed to unzip: ${zipName}`, err);
                 }
